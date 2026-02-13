cc3651faea347611f889e0b251e7a9ab9d345fbe
rawan-rady2 <142940092+rawan-rady2@users.noreply.github.com>
2026-02-14 00:33:02 +0200

    dgbbybyyb

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 const { create_user } = require('../../db/users.js')
-
+const { Login } = require('../../db/users.js')
+const jwt=require('jsonwebtoken')
 router.post('/register', async (req, res)=>{
     const {username, password} = req.body
     try{
@@ -13,4 +14,18 @@ router.post('/register', async (req, res)=>{
         console.error("Error creating user", err.stack)
     }
 })
+router.post('/login', async (req,res)=>{
+    const{username,password}=req.body
+    try{
+        const data = await Login(username,password)
+        const token=jwt.sign({id:data[0].id,username:data[0].username},process.env.JWT_SECRET,'secretkey',{expiresIn:'1h'})
+   res.status(200).json({token,username:data[0].username})
+    }
+    catch(err){
+        res.status(500).json({error: err.message})
+        console.error("Error logging in", err.stack)
+    }
+    
+})
+
 module.exports = router
--- db/users.js
+++ db/users.js
@@ -13,6 +13,22 @@ async function create_user(username, password){
         client.release()
     }
 }
+
+async function Login(username , password){
+    const client = await pool.connect()
+    try{
+        const id = await pool.query('select id,username from users where username = $1 and password = $2', [username, password])
+        return id.rows
+}
+catch(err){
+    console.error("Error executing query", err.stack)
+
+}
+finally{    client.release()
+}}
 module.exports = {
     create_user
+}
+module.exports = {
+    Login
 }
