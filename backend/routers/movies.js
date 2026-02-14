5ca53a8fe4a1ab9c18b1a2ed5f2439d8378e7990
rawan-rady2 <142940092+rawan-rady2@users.noreply.github.com>
2026-02-15 00:10:55 +0200

    ggfgfg

--- backend/routers/movies.js
+++ backend/routers/movies.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
-const {get_all_movies} = require('../../db/movies')
+const jwt = require('jsonwebtoken')
+const {get_all_movies , get_loved_movies , add_loved} = require('../../db/movies')
 
 router.get('/', async (req,res)=>{
     try{
@@ -11,5 +12,29 @@ router.get('/', async (req,res)=>{
         console.error("Error fetching movies", err.stack)
     }
 })
-
+router.get('/loved', async (req,res)=>{
+    const token = req.body.token
+    try{
+        const id = jwt.verify(token, process.env.JWT_SECRET).id
+        const movie = await get_loved_movies(id)
+        res.status(200).json(movie)
+    }
+    catch(err){
+        res.status(500).json({error: err.message})
+        console.error("Error fetching loved movies", err.stack)
+    }
+})
+router.post('/loved',async(req,res)=>{
+    const token = req.body.token
+    const movie_id = req.body.movie_id
+    try{
+        const id = jwt.verify(token, process.env.JWT_SECRET).id
+        await add_loved(id,movie_id)
+        res.status(200).json({message: "Movie added to loved list"})
+    }
+    catch(err){
+        res.status(500).json({error: err.message})
+        console.error("Error adding loved movie", err.stack)
+    }
+} )
 module.exports = router
--- db/movies.js
+++ db/movies.js
@@ -14,5 +14,30 @@ async function get_all_movies(){
     }
 }
 
+async function get_loved_movies(id){
+    const client = await pool.connect()
+    try{
+    const movies = await pool.query('select movie_id from loved movies where user_id=$1', [id])
+    return movies.rows
+    }
+    catch(err){
+        console.error("Error executing query", err.stack)
+    }
+    finally{
+        client.release()
+    }
+}
 
-module.exports = {get_all_movies}
+async function add_loved(user_id,movie_id){
+    const client = await pool.connect()
+    try{
+       await pool.query('insert into loved_movies(user_id ,movie_id) values($1,$2)', [user_id,movie_id])  
+    }
+    catch(err){console.error("Error executing query", err.stack)}
+    finally{
+        client.release()
+    }
+
+}
+
+module.exports = {get_all_movies , get_loved_movies, add_loved}
